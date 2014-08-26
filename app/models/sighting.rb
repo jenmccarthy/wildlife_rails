9e26eb3f5b44a37c084e8c825e027c7273908b58
jenmccarthy <jennifernward@gmail.com>
2014-08-26 10:23:08 -0700

    validates presence and length of longitude

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,4 +1,5 @@
 class Sighting < ActiveRecord::Base
   validates :date, :presence => true
-  validates :latitude, :presence => true, :length => { minimum: 6 }
+  validates :latitude, :presence => true, :length => { minimum: 6, maximum: 10 }
+  validates :longitude, :presence => true, :length => { minimum: 6, maximum: 10 }
 end
--- spec/models/sighting_spec.rb
+++ spec/models/sighting_spec.rb
@@ -3,9 +3,25 @@ require 'rails_helper'
 describe Sighting do
   it { should validate_presence_of :date }
   it { should validate_presence_of :latitude }
+  it { should validate_presence_of :longitude }
 
-  it 'will ensure the correct length of latitude' do
+  it 'will ensure the correct length of latitude (min)' do
     test_sighting = Sighting.new({:date => '2014-08-26', :latitude => 12, :longitude => 11.012345, :quantity => 1, :species_id => 2})
+    expect(test_sighting.save).to eq false
+  end
+
+   it 'will ensure the correct length of latitude (max)' do
+    test_sighting = Sighting.new({:date => '2014-08-26', :latitude => 11211.012345, :longitude => 11.012345, :quantity => 1, :species_id => 2})
+    expect(test_sighting.save).to eq false
+  end
+
+  it 'will ensure the correct length of longitude (min)' do
+    test_sighting = Sighting.new({:date => '2014-08-26', :longitude => 11.012345, :longitude => 11, :quantity => 1, :species_id => 2})
+    expect(test_sighting.save).to eq false
+  end
+
+   it 'will ensure the correct length of longitude (max)' do
+    test_sighting = Sighting.new({:date => '2014-08-26', :longitude => 11.012345, :longitude => 11211.012345, :quantity => 1, :species_id => 2})
     expect(test_sighting.save).to eq false
   end
 
