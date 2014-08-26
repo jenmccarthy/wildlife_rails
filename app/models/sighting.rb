68ba531a12589d416636c25ed569626a1176efe6
jenmccarthy <jennifernward@gmail.com>
2014-08-26 10:16:34 -0700

    validates presence of latitude and minimum character input in sighting

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,3 +1,4 @@
 class Sighting < ActiveRecord::Base
   validates :date, :presence => true
+  validates :latitude, :presence => true, :length => { minimum: 6 }
 end
--- spec/models/sighting_spec.rb
+++ spec/models/sighting_spec.rb
@@ -2,5 +2,11 @@ require 'rails_helper'
 
 describe Sighting do
   it { should validate_presence_of :date }
+  it { should validate_presence_of :latitude }
+
+  it 'will ensure the correct length of latitude' do
+    test_sighting = Sighting.new({:date => '2014-08-26', :latitude => 12, :longitude => 11.012345, :quantity => 1, :species_id => 2})
+    expect(test_sighting.save).to eq false
+  end
 
 end
