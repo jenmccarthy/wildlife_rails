44220377c50bdd9ab6a55dde82264df2c0e7c9a5
jenmccarthy <jennifernward@gmail.com>
2014-08-27 16:14:49 -0700

    make sighting new work for region

--- app/controllers/sighting_controller.rb
+++ app/controllers/sighting_controller.rb
@@ -4,28 +4,36 @@ class SightingController < ApplicationController
     if params[:region_id] == nil
       @species = Species.find(params[:species_id])
       @sightings = Sighting.where(:species_id => @species.id)
-      render('sightings/index.html.erb')
     elsif params[:species_id] == nil
       @region = Region.find(params[:region_id])
       @sightings = Sighting.where(:region_id => @region.id)
-      render('sightings/index.html.erb')
     end
+      render('sightings/index.html.erb')
   end
 
   def new
-    @species = Species.find(params[:species_id])
-    @sighting = @species.sightings.new
+    if params[:region_id] == nil
+      @species = Species.find(params[:species_id])
+      @sighting = @species.sightings.new
+    elsif params[:species_id] == nil
+      @region = Region.find(params[:region_id])
+      @sighting = @region.sightings.new
+    end
     render('sightings/new.html.erb')
   end
 
   def create
-    @species = Species.find(params[:species_id])
-    @sighting = @species.sightings.create(params[:sighting])
-    if @sighting.valid?
-      render('sightings/success.html.erb')
-    else
-      render('sightings/new.html.erb')
+    if params[:region_id] == nil
+      @species = Species.find(params[:species_id])
+    elsif params[:species_id] == nil
+      @region = Region.find(params[:region_id])
     end
+      @sighting = @species.sightings.create(params[:sighting])
+      if @sighting.valid?
+        render('sightings/success.html.erb')
+      else
+        render('sightings/new.html.erb')
+      end
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -15,12 +15,12 @@ Rails.application.routes.draw do
   match('species/:species_id/sightings/:id', {:via => [:patch, :put], :to => 'sighting#update'})
   match('species/:species_id/sightings/:id', {:via => :delete, :to => 'sighting#delete'})
 
-  match('region/:region_id/sightings', {:via => :get, :to => 'sighting#index'})
-  match('region/:region_id/sightings/new', {:via => :get, :to => 'sighting#new'})
-  match('region/:region_id/sightings', {:via => :post, :to => 'sighting#create'})
-  match('region/:region_id/sightings/:id/edit', {:via => :get, :to => 'sighting#edit'})
-  match('region/:region_id/sightings/:id', {:via => [:patch, :put], :to => 'sighting#update'})
-  match('region/:region_id/sightings/:id', {:via => :delete, :to => 'sighting#delete'})
+  match('regions/:region_id/sightings', {:via => :get, :to => 'sighting#index'})
+  match('regions/:region_id/sightings/new', {:via => :get, :to => 'sighting#new'})
+  match('regions/:region_id/sightings', {:via => :post, :to => 'sighting#create'})
+  match('regions/:region_id/sightings/:id/edit', {:via => :get, :to => 'sighting#edit'})
+  match('regions/:region_id/sightings/:id', {:via => [:patch, :put], :to => 'sighting#update'})
+  match('regions/:region_id/sightings/:id', {:via => :delete, :to => 'sighting#delete'})
 
   match('regions', {:via => :get, :to => 'region#index'})
   match('regions/new', {:via => :get, :to => 'region#new'})
