066b8290a42a9692dee866a49df942cd1577f996
jenmccarthy <jennifernward@gmail.com>
2014-08-27 11:33:35 -0700

    add species has many regions through sightings association

--- app/models/species.rb
+++ app/models/species.rb
@@ -3,4 +3,6 @@ class Species < ActiveRecord::Base
 
   has_many :sightings
 
+  has_many :regions, through: :sightings
+
 end
--- spec/models/species_spec.rb
+++ spec/models/species_spec.rb
@@ -4,4 +4,6 @@ describe Species do
   it { should validate_presence_of :name }
 
   it { should have_many :sightings }
+
+  it { should have_many :regions }
 end
